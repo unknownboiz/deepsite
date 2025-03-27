04ad8f7ca5251b583303868be9e867adb3ce84f1
enzo <eesteves@hotmail.fr>
2025-03-27 16:52:37 -0400

    logo

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,4 +1,4 @@
-import SpaceIcon from "@/assets/space.svg";
+import Logo from "@/assets/logo.svg";
 import { ReactNode } from "react";
 
 function Header({ children }: { children?: ReactNode }) {
@@ -7,8 +7,8 @@ function Header({ children }: { children?: ReactNode }) {
       <div className="flex items-center justify-start gap-3">
         <h1 className="text-white text-lg lg:text-xl font-bold flex items-center justify-start">
           <img
-            src={SpaceIcon}
-            alt="Space Icon"
+            src={Logo}
+            alt="DeepSite Logo"
             className="size-6 lg:size-8 mr-2"
           />
           DeepSite
